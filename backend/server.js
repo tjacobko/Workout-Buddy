ade30792f68399005da47429e2890db98af9f0e2
Jacob Ko <tko003@ucr.edu>
2023-04-12 11:32:30 -0700

    Initial Commit - Backend

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+require('dotenv').config()
+
+const express = require('express')
+const mongoose = require('mongoose')
+const workoutRoutes = require('./routes/workout')
+
+// express app
+const app = express()
+
+// middleware (happens between request and response)
+app.use(express.json())
+
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+})
+
+// routes
+app.use('/api/workouts', workoutRoutes)
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        // listen for requests
+        app.listen(process.env.PORT, () => {
+            console.log('listening on port', process.env.PORT)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
